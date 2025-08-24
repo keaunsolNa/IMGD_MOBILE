2ab27492d0d8fdd55f3e2c0af4eb8088a334b5d3
nks <knsol19921@gmail.com>
2025-08-24 15:00:55 +0900

    그룹 선택 시 해당 그룹 가입 유저 목록 보여주는 기능 추가, 해당 유저 선택 시 유저 정보 띄워주는 임시 화면 추가, 로그아웃 및 마이페이지 아이콘 추가

--- src/components/TextField.tsx
+++ src/components/TextField.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { TextInput } from 'react-native';
-import { styles } from '../styles/components/TextField';
+import { styles } from '@/styles/components/TextField';
 
 export default function TextField(props: React.ComponentProps<typeof TextInput>) {
     return <TextInput placeholderTextColor="#999" style={styles.input} {...props} />;
